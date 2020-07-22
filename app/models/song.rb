e70e5bda19fb6375f7790039bad7bfb3b642dc9c
Parker Williamson <parker.williamson@gmail.com>
2020-07-22 05:53:39 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,20 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :genre
+  belongs_to :artist
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_ids=(contents)
+      contents.each do |c|
+        note = Note.create(content: c)
+        self.notes << note
+      end
+  end
 end
